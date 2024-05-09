eb05be3ae865cf34b45d36659cf299a8e2f2e94e
Urkagurka <143172181+Urkagurka@users.noreply.github.com>
2024-05-09 16:57:16 +0200

    Add project files.

---
+++ Konstruktorerövningar4inl/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konstruktorerövning3inl
+{
+    class Person
+    {
+        private string förnamn;
+        private string efternamn;
+        private string fullnamn;
+        private DateTime födelsedatum;
+        private float längd;
+        private float vikt;
+
+        public Person(string F, string E, int födelseår, int födelsemånad, int födelsedag, float höjd, float brädd)
+        {
+            förnamn = F;
+            efternamn = E;
+            längd = höjd;
+            vikt = brädd;
+            födelsedatum = new DateTime(födelseår, födelsemånad, födelsedag);
+            fullnamn = förnamn + " " + efternamn;
+        }
+        public string getFullnamn() { return fullnamn; }
+
+        public bool isMyndig()
+        {
+            DateTime eiten = DateTime.Today.AddYears(-18);
+            if (födelsedatum.CompareTo(eiten) <= 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public float getBMI()
+        {
+            return vikt / (längd * längd);
+        }
+    }
+}
---
+++ Konstruktorerövningar4inl/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.Intrinsics.X86;
+
+namespace Konstruktorerövning3inl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person jag = new Person("alex", "ved", 2001, 09, 11, 1.46F, 135);
+            if (jag.isMyndig())
+            {
+                Console.WriteLine(jag.getFullnamn() + " är myndig");
+            }
+            else
+            {
+                Console.WriteLine(jag.getFullnamn() + " är inte myndig");
+            }
+            Console.WriteLine("Personens BMI är " + jag.getBMI());
+        }
+    }
+}
